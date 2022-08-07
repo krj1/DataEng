5f734c05d8861285bb1087e81c885146276cba32
Kurt Jackson <16569260+krj1@users.noreply.github.com>
2022-08-07 18:36:40 -0500

    made view port

--- premake5.lua
+++ premake5.lua
@@ -1,10 +1,10 @@
 -- premake5.lua
-workspace "WalnutApp"
+workspace "DataEng"
    architecture "x64"
    configurations { "Debug", "Release", "Dist" }
-   startproject "WalnutApp"
+   startproject "DataEng"
 
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 include "Walnut/WalnutExternal.lua"
 
-include "WalnutApp"
+include "DataEng"
